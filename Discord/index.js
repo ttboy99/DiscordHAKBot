3d98341e197d98c17ef5e4b204bdd568cfeb65ca
tautod15 <tautod15@htlkaindorf.at>
2020-10-15 22:55:13 +0200

    Removed Purged

--- Discord/index.js
+++ Discord/index.js
@@ -130,21 +130,7 @@ client.on("message", async message => {
     message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
   }
   
-  if(command === "purge") {
-    // This command removes all messages from all users in the channel, up to 100.
     
-    // get the delete count, as an actual number.
-    const deleteCount = parseInt(args[0], 10);
-    
-    // Ooooh nice, combined conditions. <3
-    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
-      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
-    
-    // So we get our messages, and delete them. Simple enough, right?
-    const fetched = await message.channel.fetchMessages({limit: deleteCount});
-    message.channel.bulkDelete(fetched)
-      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
-  }
 
 	if(command === "spam"){
 		spammessage = args.join(" ");
